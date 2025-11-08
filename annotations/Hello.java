0170e5a0e03db143c06e18c8bf63ce41ce596ae8
singhnavin19 <singhnavin19290@gmail.com>
2025-11-08 17:10:00 +0530

    lecture1

---
+++ annotations/Hello.java
@@ -0,0 +1,11 @@
+package annotations;
+@MyFirstAnnoation
+public class Hello {
+    @MyFirstAnnoation
+    public int i=10;
+    @Override
+    @MyFirstAnnoation
+    static void main(String[] args) {
+        System.out.println("hello");
+    }
+}
